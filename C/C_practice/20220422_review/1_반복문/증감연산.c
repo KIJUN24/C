952ec6968e288195dbfbc0038f9565d188bd8ae9
KIJUN24 <97867568+KIJUN24@users.noreply.github.com>
2022-04-23 00:18:54 +0900

    4 commit

---
+++ C/C_practice/20220422_review/1_반복문/증감연산.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	// printf("Hello World\n");
+	
+	int a = 20;
+	
+	printf("%d\n", ++a);	// 1을 증감 후 출력
+	printf("%d\n", a++);	// 출력한 후 1 증감
+	printf("%d\n", a);
+	return 0;
+}
